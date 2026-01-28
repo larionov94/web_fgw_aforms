f8cb3f2816e774d5c3a3a4287904408708eafe06
larionov94 <anatolii.larionov94@yandex.ru>
2026-01-28 14:36:30 +0400

    0051 - Добавить в форму добавления продукции -> создание на основе этой продукции

--- internal/handler/http_web/aforms/production_handler_http.go
+++ internal/handler/http_web/aforms/production_handler_http.go
@@ -341,6 +341,39 @@ func (p *ProductionHandlerHTML) handlerGetAddForm(w http.ResponseWriter, r *http
 		return
 	}
 
+	if r.URL.Query().Get("basedOn") != "" {
+		fmt.Println("2")
+		basedOnProduction, err := p.ensureProductionExists(r, "basedOn")
+		if err != nil {
+			http_err.SendErrorHTTP(w, http.StatusNotFound, msg.H7000+err.Error(), p.logg, r)
+
+			return
+		}
+		fmt.Println("2", basedOnProduction.IdProduction, basedOnProduction.PrPackName)
+
+		fmt.Println("1")
+
+		basedOnProduction.IdProduction = 0
+		basedOnProduction.PrArticle = ""
+
+		data := page.NewDataPage(
+			addPageTitle,
+			addPageKey,
+			performerData,
+			[]*model.Production{basedOnProduction},
+			nil,
+			nil,
+			false,
+			designNameList,
+			colorList,
+		)
+
+		page.RenderPages(w, tmplIndexHTML, data, r, tmplProductionHTML, tmplProductionAddHTML, tmplProductionUpdHTML)
+
+		return
+
+	}
+
 	data := page.NewDataPage(
 		addPageTitle,
 		addPageKey,
